99800c3347f8682c03047563f4764276cbbffb82
Van Nguyen <93vannd@gmail.com>
2020-06-26 11:32:55 +0300

    remove comments

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -20,14 +20,4 @@ export const authJWT = (req, res, next) => {
   } else {
     res.sendStatus(401)
   }
-  // const token = req.header('token')
-  // if (!token) return res.status(401).json({ message: 'Auth Error' })
-  // try {
-  //   const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  //   req.user = decoded.user
-  //   next()
-  // } catch (e) {
-  //   console.error(e)
-  //   res.status(500).send({ message: 'Invalid Token' })
-  // }
 }
